5ff41613054843af52e55ac8a252f33e88b92828
maheshkumba536-mk <maheshkumba536@gmail.com>
2026-04-22 14:53:03 +0530

    Set DEBUG to True for development

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7p2*b0q-qe)9bn1t^)*-!&@6%o&)xyokii9t5%zfn5_(euuuu1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
