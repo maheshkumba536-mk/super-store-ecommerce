19ae227ac0de9b1303fec9678d9cd43aa8eac2ff
Mahi <maheshkumba536@gmail.com>
2026-05-11 16:40:44 +0530

    Add Cloudinary media storage

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -49,8 +49,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',          # must be before staticfiles
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'cloudinary',
     'store',
 ]
 
@@ -136,6 +138,30 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'store' / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# ---------------------------------------------------------------------------
+# Cloudinary – media file storage
+# Set CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET
+# as environment variables (in .env.local for dev, Render dashboard for prod).
+# ---------------------------------------------------------------------------
+import cloudinary
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', ''),
+    'API_KEY':    os.environ.get('CLOUDINARY_API_KEY', ''),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', ''),
+}
+
+cloudinary.config(
+    cloud_name=CLOUDINARY_STORAGE['CLOUD_NAME'],
+    api_key=CLOUDINARY_STORAGE['API_KEY'],
+    api_secret=CLOUDINARY_STORAGE['API_SECRET'],
+    secure=True,
+)
+
+# Use Cloudinary for uploaded media files (ImageField, FileField, etc.)
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+# Keep these for local dev fallback / Django compatibility
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
